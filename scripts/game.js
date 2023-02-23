de74642da9076c9c13ba73c6d674fa45dcbb2633
Alena18 <sun180983@gmail.com>
2023-02-23 19:47:58 +0000

    Add addTurn, showScore function

--- scripts/game.js
+++ scripts/game.js
@@ -6,5 +6,22 @@ let game = {
 
 };
 
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+function addTurn() {
+     game.playerMoves = [];
+     game.currentGame.push(game.choices[(Math.floor(Math.random() *4))]);
+    //  showTurns();
+}
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+
+module.exports = { game, newGame, showScore, addTurn }; 
